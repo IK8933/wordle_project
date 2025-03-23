70768eefc992e9777b4d5d21181c86a5310581be
IK8933 <iankessack1989@gmail.com>
2025-03-23 15:13:37 -0400

    added tsconfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,15 @@
-import React, { useState } from "react";
 import Header from "./components/Header";
 import GameBoard from "./components/GameBoard";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer"; 
 
 function App() {
-  const [gameKey, setGameKey] = useState(0);
-
-  const resetGame = () => {
-    setGameKey((prevKey) => prevKey + 1);
-  };
-
   return (
     <div className="flex flex-col items-center min-h-screen">
-      <Header resetGame={resetGame} />
-      <GameBoard key={gameKey} />
-      <Footer />
+      <Header />
+      <GameBoard />
+      <Footer /> 
     </div>
   );
 }
 
 export default App;
-
-
-
-
-// import Header from "./components/Header";
-// import GameBoard from "./components/GameBoard";
-// import Footer from "./components/Footer"; 
-
-// function App() {
-//   return (
-//     <div className="flex flex-col items-center min-h-screen">
-//       <Header />
-//       <GameBoard />
-//       <Footer /> 
-//     </div>
-//   );
-// }
-
-// export default App;
